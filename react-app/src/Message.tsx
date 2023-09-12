44585e054e6efdea5a33788a825e61a3e5f579af
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-09-12 20:15:55 +0200

    bootstrap && fragment lesson

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,7 +1,7 @@
 function Message() {
   const name = 'Bassie';
   if (name)
-    return <h1>Hello {name}</h1>;
+    return <h1 className="bg-gray-800">Hello {name}</h1>;
   return <h1>Hello World</h1>;
 }
 
