d25cbba287caa1e28bb03b8200c43dc89a328840
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-09-17 18:01:20 +0200

    added show/hide all && bit styling

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -15,6 +15,7 @@ const App = () => {
 
   const [newItem, setNewItem] = useState(new Task(""));
   const [isSelecting, setIsSelecting] = useState(false);
+  const [viewAllList, setViewAllList] = useState(false);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -29,31 +30,50 @@ const App = () => {
   };
 
   return (
-    <div>
-      <Input
-        type="text"
-        placeholder="Add to to do"
-        className="w-100"
-        value={newItem.getName()}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-      />
-      <h2>Only undone tasks</h2>
-      <ToDoList
-        items={items.filter((item) => !item.getIsDone())}
-        isSelecting={isSelecting}
-        withEditOption={true}
-      />
-      <h2 className="mb-4 mt-5">Complete list</h2>
-      <ToDoList items={items} isSelecting={isSelecting} />
-      <Button
-        onClick={() => {
-          setIsSelecting(!isSelecting);
-        }}
-      >
-        Edit
-      </Button>
-    </div>
+    <main className="p-4">
+      <section className="w-100 text-center text-primary">
+        <h1>To Do App</h1>
+      </section>
+      <section className="border p-5 rounded w-50 mx-auto mt-4">
+        <Input
+          type="text"
+          placeholder="Add to to do"
+          className="w-50 position-relative"
+          value={newItem.getName()}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+        <div className="position-absolute w-50">
+          <Button
+            onClick={() => {
+              setViewAllList(!viewAllList);
+            }}
+          >
+            {viewAllList ? "Hide all list" : "Show all"}
+          </Button>
+        </div>
+        <h2>Only undone tasks</h2>
+        <ToDoList
+          items={items.filter((item) => !item.getIsDone())}
+          isSelecting={isSelecting}
+          withEditOption={true}
+        />
+        {viewAllList && (
+          <div>
+            <h2 className="mb-4 mt-5">Complete list</h2>
+            <ToDoList items={items} isSelecting={isSelecting} />
+          </div>
+        )}
+
+        <Button
+          onClick={() => {
+            setIsSelecting(!isSelecting);
+          }}
+        >
+          {isSelecting ? "Save" : "Edit"}
+        </Button>
+      </section>
+    </main>
   );
 };
 
